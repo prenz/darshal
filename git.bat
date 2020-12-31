7d11822bc983bc19d48b608ec5234514a50d8418
darshal <prenz@naver.com>
2020-12-31 09:24:01 +0900

    exploit test

---
+++ git.bat
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1ce8c31a1f3a31e4e8095c2652480e965cb617c656bdc9b476cea624168650e1
+size 1862
---
+++ revsh_powersh.ps1
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f83c80d31333f0ec6d3456fa0b7e10b9d1b8000f3632343d88513949d07985e6
+size 499
